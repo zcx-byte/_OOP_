d29567a7becd3bcedadc1e1f77716811143fc72f
Chernibog <nikitanofamily@gmail.com>
2025-12-08 22:03:59 +0900

    Add files via upload

---
+++ KlaccBank/sberbank.hpp
@@ -0,0 +1,19 @@
+// sberbank.hpp 
+
+
+#ifndef SBERBANK_HPP
+#define SBERBANK_HPP
+
+#include <string>
+using namespace std;
+
+extern string name[10000];
+extern int money[10000];
+extern int pin[10000];
+extern int card[10000];
+extern bool blocked[10000];
+extern int total;
+
+void Klacc();
+
+#endif // SBERBANK_HPP
